d7e405664fabdfd540a23e70c3ef60c1346766db
gbraux <gbraux@supinfo.com>
2016-11-13 13:53:07 +0100

    Change config for RPI3

--- www/LightControlHelper.php
+++ www/LightControlHelper.php
@@ -5,13 +5,13 @@ echo "<br>";
 
 if ($_GET['dataLine'] == "all")
 {
-	exec("sudo /home/osmc/LightControl 1 ".$_GET['onOff']);
-	exec("sudo /home/osmc/LightControl 2 ".$_GET['onOff']);
-	exec("sudo /home/osmc/LightControl 3 ".$_GET['onOff']);
-	exec("sudo /home/osmc/LightControl 4 ".$_GET['onOff']);
+	exec("sudo /home/osmc/Conde_Home_Automation/LightControl/LightControl 1 ".$_GET['onOff']);
+	exec("sudo /home/osmc/Conde_Home_Automation/LightControl/LightControl 2 ".$_GET['onOff']);
+	exec("sudo /home/osmc/Conde_Home_Automation/LightControl/LightControl 3 ".$_GET['onOff']);
+	exec("sudo /home/osmc/Conde_Home_Automation/LightControl/LightControl 4 ".$_GET['onOff']);
 }
 
-echo "sudo /home/osmc/LightControl ".$_GET['dataLine']." ".$_GET['onOff'];
+echo "sudo /home/osmc/Conde_Home_Automation/LightControl/LightControl ".$_GET['dataLine']." ".$_GET['onOff'];
 
-exec("sudo /home/osmc/LightControl ".$_GET['dataLine']." ".$_GET['onOff']);
+exec("sudo /home/osmc/Conde_Home_Automation/LightControl/LightControl ".$_GET['dataLine']." ".$_GET['onOff']);
 ?>
